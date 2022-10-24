cc70bab19b40593b26517e2a45d4b9afa1fb6370
TheWerelizard <35267405+TheWerelizard@users.noreply.github.com>
2022-10-24 14:22:12 +0200

    Added database core features and SQL alchemy

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 # Imports
-from flask import Flask, render_template, abort, redirect, url_for
+import json
+from flask import Flask, render_template, abort, redirect, request, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
  
+# Fetch configuration files
+appConfiguration =  json.load(open('appConfig/appConfiguration.json','r'))
+secretKey = appConfiguration['secretKey']
+
 # Build the app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secretKey
 app.debug = True
+
+#Database configuration
+db_name = 'ibis.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+# Tables config
+class hieroglyphs(db.Model):
+    __tablename__ = 'hieroglyphs'
+    index = db.Column(db.Integer, primary_key=True)
+    translation = db.Column(db.String)
+    transliteration = db.Column(db.String)
+    gardiner = db.Column(db.String)
+    tags = db.Column(db.String)
+    transliterationAdjacencies = db.Column(db.String)
+    notes = db.Column(db.String)
+    references = db.Column(db.String)
  
-# Ressources
+# Ressources for static pages
 @app.route('/')
 def indexReroute():
     return redirect(url_for('index'))
@@ -13,10 +39,6 @@ def indexReroute():
 @app.route('/index')
 def index():
     return render_template('index.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 @app.route('/about')
 def about():
@@ -26,8 +48,25 @@ def about():
 def references():
     return render_template('references.html')
 
-@app.route('/search/translation')
+# Ressources for form pages
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/search/translation', methods=('GET', 'POST'))
 def translation():
+    if request.method == 'POST':
+        try:
+            translationRequest = request.form['translation']
+            if translationRequest:
+                output = hieroglyphs.query.order_by(hieroglyphs.translation).all()
+                print(output)
+                return render_template('translationOutput.html',translationOutput = translationRequest)
+            else:
+                return render_template('translation.html')
+        except Exception as error:
+            error_text = str(error); print(error_text)
+            return render_template('translation.html')
     return render_template('translation.html')
 
 @app.route('/search/transliteration')
