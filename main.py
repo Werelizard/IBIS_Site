be83492ec47c5034e85027adfdec0c0164a9cd10
TheWerelizard <35267405+TheWerelizard@users.noreply.github.com>
2022-10-24 02:20:30 +0200

    Added pages, additional css and assets

--- main.py
+++ main.py
@@ -17,10 +17,26 @@ def index():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
- 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+ 
+@app.route('/references')
+def references():
+    return render_template('references.html')
+
+@app.route('/search/translation')
+def translation():
+    return render_template('translation.html')
+
+@app.route('/search/transliteration')
+def translitertion():
+    return render_template('transliteration.html')
+
+@app.route('/search/hieroglyphics')
+def hieroglyphics():
+    return render_template('hieroglyphics.html')
 
 # App runner
 if __name__ == '__main__':
