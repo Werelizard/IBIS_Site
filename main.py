7662b1caf40f9a53e743270655427e56ca070fe9
TheWerelizard <35267405+TheWerelizard@users.noreply.github.com>
2022-10-24 22:38:42 +0200

    Implemented search methods for trs and trl

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Imports
 import json
+from turtle import pos
 from flask import Flask, render_template, abort, redirect, request, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
+import sqlite3
  
 # Fetch configuration files
 appConfiguration =  json.load(open('appConfig/appConfiguration.json','r'))
@@ -13,23 +13,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = secretKey
 app.debug = True
 
-#Database configuration
+#Database connection
 db_name = 'ibis.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
 
-# Tables config
-class hieroglyphs(db.Model):
-    __tablename__ = 'hieroglyphs'
-    index = db.Column(db.Integer, primary_key=True)
-    translation = db.Column(db.String)
-    transliteration = db.Column(db.String)
-    gardiner = db.Column(db.String)
-    tags = db.Column(db.String)
-    transliterationAdjacencies = db.Column(db.String)
-    notes = db.Column(db.String)
-    references = db.Column(db.String)
+def get_db_connection():
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    return conn
  
 # Ressources for static pages
 @app.route('/')
@@ -57,21 +47,76 @@ def contact():
 def translation():
     if request.method == 'POST':
         try:
+            #Get variables from form
             translationRequest = request.form['translation']
+            try:
+                startWithQueryOnly = request.form['searchFirstMatchingOnly']; startWithQueryOnly = True
+            except:
+                startWithQueryOnly = False
+            
+            #Ping the SQL databse if argument, refresh page if nothing
             if translationRequest:
-                output = hieroglyphs.query.order_by(hieroglyphs.translation).all()
-                print(output)
-                return render_template('translationOutput.html',translationOutput = translationRequest)
-            else:
-                return render_template('translation.html')
+                conn = get_db_connection()
+                #Determine whether to look for words starting with the request or words containing the request
+                if startWithQueryOnly:
+                    output = conn.execute("SELECT * FROM hieroglyphs WHERE UPPER(hieroglyphs.translation) LIKE  UPPER('" + translationRequest.lower() +"%') OR UPPER(hieroglyphs.translationEnglish) LIKE  UPPER('" + translationRequest.lower() +"%')").fetchall()
+                else:
+                    output = conn.execute("SELECT * FROM hieroglyphs WHERE UPPER(hieroglyphs.translation) LIKE  UPPER('%" + translationRequest.lower() +"%') OR UPPER(hieroglyphs.translationEnglish) LIKE  UPPER('%" + translationRequest.lower() +"%')").fetchall()
+                conn.close()
+
+                #Repackage the query results into a dictionary for later iterations
+                if len(output) < 1:
+                    entries = 'None'
+                else:
+                    entries = {}; index = 0
+                    for entry in output:
+                        entries[index] = {'translation': entry['translation'],'translationEnglish': entry['translationEnglish'],'transliteration': entry['transliteration'],'gardiner':entry['gardiner'],'tags':entry['tags'],'references':entry['references'],'notes':entry['notes']}
+                        index = index + 1
+
+                #Return template
+                return render_template('translationOutput.html', entries = entries)
         except Exception as error:
             error_text = str(error); print(error_text)
             return render_template('translation.html')
     return render_template('translation.html')
 
-@app.route('/search/transliteration')
+@app.route('/search/transliteration', methods=('GET', 'POST'))
 def translitertion():
+    if request.method == 'POST':
+        try:
+            #Get variables from form
+            transliterationRequest = request.form['transliteration']
+            try:
+                startWithQueryOnly = request.form['searchFirstMatchingOnly']; startWithQueryOnly = True
+            except:
+                startWithQueryOnly = False
+            
+            #Ping the SQL databse if argument, refresh page if nothing
+            if transliterationRequest:
+                conn = get_db_connection()
+                #Determine whether to look for words starting with the request or words containing the request
+                if startWithQueryOnly:
+                    output = conn.execute("SELECT * FROM hieroglyphs WHERE UPPER(hieroglyphs.transliteration) LIKE  UPPER('" + transliterationRequest.lower() +"%') OR UPPER(hieroglyphs.translationEnglish) LIKE  UPPER('" + transliterationRequest.lower() +"%')").fetchall()
+                else:
+                    output = conn.execute("SELECT * FROM hieroglyphs WHERE UPPER(hieroglyphs.transliteration) LIKE  UPPER('%" + transliterationRequest.lower() +"%') OR UPPER(hieroglyphs.translationEnglish) LIKE  UPPER('%" + transliterationRequest.lower() +"%')").fetchall()
+                conn.close()
+
+                #Repackage the query results into a dictionary for later iterations
+                if len(output) < 1:
+                    entries = 'None'
+                else:
+                    entries = {}; index = 0
+                    for entry in output:
+                        entries[index] = {'translation': entry['translation'],'translationEnglish': entry['translationEnglish'],'transliteration': entry['transliteration'],'gardiner':entry['gardiner'],'tags':entry['tags'],'references':entry['references'],'notes':entry['notes']}
+                        index = index + 1
+
+                #Return template
+                return render_template('transliterationOutput.html', entries = entries)
+        except Exception as error:
+            error_text = str(error); print(error_text)
+            return render_template('transliteration.html')
     return render_template('transliteration.html')
+    
 
 @app.route('/search/hieroglyphics')
 def hieroglyphics():
